e72925e5ec4b878f71c302c4296af440d4c351ea
JMJ <narry46@sookmyung.ac.kr>
2023-01-27 15:14:25 +0900

    test

---
+++ joinus/src/main/java/io/codee/JoinusApplication.java
@@ -0,0 +1,13 @@
+package io.codee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinusApplication.class, args);
+	}
+
+}
