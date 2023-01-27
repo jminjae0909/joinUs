30af0e1ca5ef68bfae60ac4aba8d149a42bc2771
JMJ <narry46@sookmyung.ac.kr>
2023-01-27 15:26:22 +0900

    test

---
+++ joinus/src/test/java/io/codee/JoinusApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
